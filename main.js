064e18576a7cf1c0307762f750e46bc3d5e1918d
frontendsara <sara.karlsson1991@hotmail.com>
2022-11-28 16:23:24 +0100

    added some styling, changed js

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-let mainNav = document.getElementById("nav");
-let navBarToggle = document.getElementById("hb-btn");
 
-navBarToggle.addEventListener("click", function(){
+window.addEventListener('DOMContentLoaded', main);
+
+function main(){
+    addEventListeners();
+}
+
+function addEventListeners (){
+    let navBarToggle = document.getElementById("hb-btn");
+    navBarToggle.onclick = mainNav;
+}
+
+function mainNav (){
+    let ul = document.querySelector("ul");
+    ul.classList.toggle("active");
+    
+}
+
+//spara, original burgermenu
+/*let mainNav = document.getElementById("nav");
+let navBarToggle = document.getElementById("hb-btn");*/
+
+/*navBarToggle.addEventListener("click", function(){
     mainNav.classList.toggle("active");
-});
+});*/
