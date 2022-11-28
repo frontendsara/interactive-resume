fd8336c3a49a22c915c227cb376c7eb1606ea656
frontendsara <sara.karlsson1991@hotmail.com>
2022-11-28 10:21:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let mainNav = document.getElementById("nav");
+let navBarToggle = document.getElementById("hb-btn");
+
+navBarToggle.addEventListener("click", function(){
+    mainNav.classList.toggle("active");
+});
