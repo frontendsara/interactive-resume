b8e42c48643ee34a7e5d7bcad2da38566c374e55
frontendsara <sara.karlsson1991@hotmail.com>
2022-11-30 17:32:51 +0100

    on scroll function

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 window.addEventListener('DOMContentLoaded', main);
+window.addEventListener("scroll", reveal);
+
 /**Program starts */
 function main(){
-    addEventListeners();  
+    addEventListeners();
+    reveal () 
 }
 
+/**A function that handle the clicks on the webpage*/
 function addEventListeners () {
     const navBarToggle = document.getElementById("hb-btn");
     navBarToggle.addEventListener("click", toggleNav);
@@ -14,18 +18,37 @@ function addEventListeners () {
     }
 }
 
+/**Function who calls other functions */
 function handleNavItemClick (event) {
     getToNavItem(event);
     toggleNav();
 }
 
+/**A function that get us to the selectet part of the menu*/
 function getToNavItem(event){
     const activeLI = document.querySelector("header nav li");
     const li = event.currentTarget;
 }
 
+/**A function that show the hamburgermenu*/
 function toggleNav (){
     const ul = document.querySelector("ul");
-    ul.classList.toggle("active");
-    
+    ul.classList.toggle("active"); 
+}
+
+/**A function that reveals content when scroll up and down on the page*/
+function reveal () {
+    const reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++){
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }else {
+            reveals[i].classList.remove("active");
+        }
+    }
 }
